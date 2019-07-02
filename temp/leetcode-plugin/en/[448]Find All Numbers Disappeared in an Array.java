b35286e74761007aecd197662b19148867f342ca
Feier Xiao <feier.xiao@anu.edu.au>
2019-07-02 23:26:12 +1000

    Day 5

--- temp/leetcode-plugin/en/[448]Find All Numbers Disappeared in an Array.java
+++ temp/leetcode-plugin/en/[448]Find All Numbers Disappeared in an Array.java
@@ -16,6 +16,17 @@
 
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        
+        List<Integer> list = new LinkedList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[Math.abs(nums[i]) - 1] > 0) {
+                nums[Math.abs(nums[i]) - 1] = - nums[Math.abs(nums[i]) - 1];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                list.add(i + 1);
+            }
+        }
+        return list;
     }
 }
